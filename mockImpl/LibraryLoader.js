8ae4090e8ae69156faf8abc1911a90c9ec77bc7b
Enrico <enrico.mazzucchelli@vipera.com>
2017-08-10 12:34:26 +0200

    update library loader

--- mockImpl/LibraryLoader.js
+++ mockImpl/LibraryLoader.js
@@ -7,22 +7,52 @@ module.exports = function(reload,serviceBridge){
      * such as persistenceService (aka NativeStorage) or
      * localStorage emulator (aka DEStorage)
      */
+
+     /* native storage available api
+        getItem(key:string,success:Function,fail:Function):void
+        setItem(key:string,value,success:Function,fail:Function):void
+        deleteItem(key:string,success:Function,fail:Function):void
+        clear(success:Function,fail:Function):void
+     */
     var toolsNativeStorage = serviceBridge.getService("nativeStorage");
+
+
+    /* localStorage storage available api
+        set(key:string, value):void
+        setObject(key:string, value:Object):void
+        get(key:string):string
+        getObject(key:string):Object
+        remove(key):void
+        clear():void
+        getLength():number
+        printAllData():void
+     */
     var toolsDEStorage = serviceBridge.getService("localStorage");
+
+    // NOT YET AVAILABLE
     var toolsUtilities = serviceBridge.getService("Utilities");
 
+    // load your library local
+    //
     //var libraryPath="./www/lib/";
     //moment = require(libraryPath + 'moment.js');
     //_ = require(libraryPath + 'lodash.min.js');
 
+
+    // or via npm:
+    //_ = require('lodash');
+
     //Define your custom adapter here (N.B: define adapter globally);
-    /* example: support native storage set and getString api:*/
+    /* example: NativeStora DE implementation:*/
     NativeStorage = {
         set:function(key,value,success,fail){
             toolsNativeStorage.setItem(key,value,success,fail);
         },
         get:function(key,success,fail){
             toolsNativeStorage.getItem(key,success,fail);
+        },
+        clear:function(success,fail){
+          toolsNativeStorage.clear(success,fail);
         }
     };
 
